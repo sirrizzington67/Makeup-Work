1394fcc70318ffe1f875bf9910fef6c46a9a6ec6
sirrizzington67 <zielinskI.a@northeastern.edu>
2026-04-23 21:47:51 -0400

    First Assignment Done

---
+++ MakeUp-Work/InClassCoding.cpp
@@ -0,0 +1,24 @@
+/*
+# Makeup-Work
+1. 12 35
+2. You entered: 19
+3. 2.8714
+4. 
+x + y = 11
+x - y = 5
+x * y = 24
+x / y = 2
+x % y = 2
+5. 
+A. It's hot outside!
+B. Wednesday
+C. 
+Count: 1
+Count: 2
+Count: 3
+Count: 4
+Count: 5
+6. Hello from a function!
+7. Sum = 10
+8. Area = 20 (assuming width == 4 and height == 5)
+*/
